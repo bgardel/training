8946d5731bd4f18ddec88de4dc88d77b30ffb841
Brian <bgardel@advdigitalconsulting.com>
2017-12-01 11:46:49 -0700

    admin function tweaks - alecadd part 4

--- wp-content/themes/adc-theme/inc/function-admin.php
+++ wp-content/themes/adc-theme/inc/function-admin.php
@@ -7,16 +7,34 @@
 
 function adc_add_admin_page() {
     //Generate Custom Admin Page
-    add_menu_page( 'ADC Theme Options', 'ADC', 'manage_options', 'adc_options', 'adc_theme_create_page', 'dashicons-admin-customizer', 110 );
+    add_menu_page('ADC Theme Options', 'ADC', 'manage_options', 'adc_options', 'adc_theme_create_page', 'dashicons-admin-customizer', 110);
 
     //Generate Custom Admin Sub pages
     add_submenu_page('adc_options', 'ADC Theme Options', 'General', 'manage_options', 'adc_options', 'adc_theme_create_page');
     add_submenu_page('adc_options', 'ADC CSS Options', 'Custom CSS', 'manage_options', 'adc_options_css', 'adc_theme_settings_page');
+
+    //Activate custom settings
+    add_action('admin_init', 'adc_custom_settings');
 }
 add_action('admin_menu', 'adc_add_admin_page');
 
+function adc_custom_settings() {
+    register_setting('adc-settings-group', 'first_name');
+    add_settings_section('adc-sidebar-options', 'Sidebar Options', 'adc_sidebar_options', 'adc_options');
+    add_settings_field('sidebar-name', 'First Name', 'adc_sidebar_name', 'adc_options', 'adc-sidebar-options');
+}
+
+function adc_sidebar_options() {
+    echo 'Customize your sidebar information';
+}
+
+function adc_sidebar_name() {
+    $firstName = esc_attr(get_option('first_name'));
+    echo '<input type="text" name="first_name" value="'.$firstName.'" placeholder="First Name" />';
+}
+
 function adc_theme_create_page() {
-    require_once( get_template_directory() . '/inc/templates/adc-admin.php' );
+    require_once(get_template_directory() . '/inc/templates/adc-admin.php');
 }
 
 function adc_theme_settings_page() {
--- wp-content/themes/adc-theme/inc/templates/adc-admin.php
+++ wp-content/themes/adc-theme/inc/templates/adc-admin.php
@@ -1,4 +1,7 @@
 <h1>ADC Theme Options</h1>
-<h3 class="title">Manage Options</h3>
-<p>Customize the default WordPress Appearance Options</p>
-<?php bloginfo('name') ?>
+<?php settings_errors(); ?>
+<form method="post" action="options.php">
+    <?php settings_fields('adc-settings-group'); ?>
+    <?php do_settings_sections('adc_options') ?>
+    <?php submit_button(); ?>
+</form>
