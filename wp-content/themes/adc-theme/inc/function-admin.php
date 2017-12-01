2baf27ba80533297789dd783f6b93faf22d5e5fd
Brian <bgardel@advdigitalconsulting.com>
2017-12-01 10:40:36 -0700

    create admin template folder and tweak admin functions - alecadd part 3

--- wp-content/themes/adc-theme/inc/function-admin.php
+++ wp-content/themes/adc-theme/inc/function-admin.php
@@ -6,10 +6,19 @@
 */
 
 function adc_add_admin_page() {
-    add_menu_page( 'ADC Theme Options', 'ADC', 'manage_options', 'adc', 'adc_theme_create_page', 'dashicons-admin-customizer', 110 );
+    //Generate Custom Admin Page
+    add_menu_page( 'ADC Theme Options', 'ADC', 'manage_options', 'adc_options', 'adc_theme_create_page', 'dashicons-admin-customizer', 110 );
+
+    //Generate Custom Admin Sub pages
+    add_submenu_page('adc_options', 'ADC Theme Options', 'General', 'manage_options', 'adc_options', 'adc_theme_create_page');
+    add_submenu_page('adc_options', 'ADC CSS Options', 'Custom CSS', 'manage_options', 'adc_options_css', 'adc_theme_settings_page');
 }
 add_action('admin_menu', 'adc_add_admin_page');
 
 function adc_theme_create_page() {
-    echo '<h1>ADC Theme Options</h1>';
+    require_once( get_template_directory() . '/inc/templates/adc-admin.php' );
+}
+
+function adc_theme_settings_page() {
+    echo '<h1>Custom CSS</h1>';
 }
